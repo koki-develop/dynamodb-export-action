709d44d568dd1eaf6c9341908004ed5d009c89d7
koki-develop <kou.pg.0131@gmail.com>
2024-08-10 16:39:28 +0900

    Implement action

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import * as core from '@actions/core'
+import {
+  DescribeExportCommand,
+  DescribeTableCommand,
+  DynamoDBClient,
+  ExportFormat,
+  ExportStatus,
+  ExportTableToPointInTimeCommand
+} from '@aws-sdk/client-dynamodb'
 
 export async function run(): Promise<void> {
   try {
@@ -7,13 +15,58 @@ export async function run(): Promise<void> {
     const s3Prefix = core.getInput('s3-prefix')
     const exportFormat = core.getInput('export-format')
 
+    switch (exportFormat) {
+      case ExportFormat.DYNAMODB_JSON:
+      case ExportFormat.ION:
+        break
+      default:
+        throw new Error(`Unsupported export format: ${exportFormat}`)
+    }
+
+    const client = new DynamoDBClient()
+
+    const describeTableResponse = await client.send(
+      new DescribeTableCommand({ TableName: table })
+    )
+    const tableDetail = describeTableResponse.Table!
+
     core.info(
       `Exporting table ${table} to s3://${s3Bucket}/${s3Prefix} in ${exportFormat} format.`
     )
 
-    // TODO
+    const exportResponse = await client.send(
+      new ExportTableToPointInTimeCommand({
+        TableArn: tableDetail.TableArn,
+        S3Bucket: s3Bucket,
+        S3Prefix: s3Prefix,
+        ExportFormat: exportFormat
+      })
+    )
+    const exportArn = exportResponse.ExportDescription!.ExportArn
 
-    core.setOutput('export-arn', 'EXPORT_ARN')
+    let status: ExportStatus = ExportStatus.IN_PROGRESS
+    do {
+      const describeExportResponse = await client.send(
+        new DescribeExportCommand({ ExportArn: exportArn })
+      )
+
+      const exportDetail = describeExportResponse.ExportDescription!
+      status = exportDetail.ExportStatus!
+
+      switch (status) {
+        case ExportStatus.IN_PROGRESS:
+          core.info('Export in progress...')
+          await new Promise(resolve => setTimeout(resolve, 5000))
+          break
+        case ExportStatus.COMPLETED:
+          core.info('Export completed.')
+          break
+        case ExportStatus.FAILED:
+          throw new Error(`Export failed: ${exportDetail.FailureMessage}`)
+      }
+    } while (status === ExportStatus.IN_PROGRESS)
+
+    core.setOutput('export-arn', exportArn)
     core.setOutput('export-manifest', 'EXPORT_MANIFEST')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
